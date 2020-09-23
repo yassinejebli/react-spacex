d71374927284440a65c7a95baca0fa8ee381e0d0
Yassine Jebli <yassine@mobilly.lv>
2020-09-23 13:19:16 +0300

    Fix share launch data actions name

--- src/actions/shareLaunchDataActions.js
+++ src/actions/shareLaunchDataActions.js
@@ -1,6 +1,6 @@
-export const SHARE_DATA_BEGIN = "SHARE_ORBITS_DATA_BEGIN";
-export const SHARE_DATA_SUCCESS = "SHARE_ORBITS_DATA_SUCCESS";
-export const SHARE_DATA_FAIL = "SHARE_ORBITS_DATA_FAIL";
+export const SHARE_DATA_BEGIN = "SHARE_LAUNCH_DATA_BEGIN";
+export const SHARE_DATA_SUCCESS = "SHARE_LAUNCH_DATA_SUCCESS";
+export const SHARE_DATA_FAIL = "SHARE_LAUNCH_DATA_FAIL";
 
 export function shareLaunchData(launchData) {
   return {
